5ccd5c859be8de5738d261b3a70155154614fc5d
kangminchan99 <114412280+kangminchan99@users.noreply.github.com>
2023-06-06 00:13:13 +0900

    main_page

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import 'blog_page.dart';
 import 'image_page.dart';
 import 'vedio_page.dart';
@@ -14,15 +13,33 @@ class MainPage extends StatefulWidget {
 class _MainPageState extends State<MainPage>
     with SingleTickerProviderStateMixin {
   late TabController tabController = TabController(length: 3, vsync: this);
+  TextEditingController textEditingController = TextEditingController();
+  String blogText = '';
+
+  void update() => setState(() {});
+
+  void handleSearch() {
+    update();
+    blogText = textEditingController.text; // 텍스트 값 저장
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: TextFormField(),
+        title: TextFormField(
+          controller: textEditingController,
+        ),
         leading: Image.asset('images/dlogo.jpeg'),
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: handleSearch,
+            // Navigator.push(
+            //   context,
+            //   MaterialPageRoute(
+            //     builder: (context) => BlogPage(blogText: blogText),
+            //   ),
+            // );
             icon: const Icon(Icons.search),
           ),
         ],
@@ -40,10 +57,12 @@ class _MainPageState extends State<MainPage>
           Expanded(
             child: TabBarView(
               controller: tabController,
-              children: const [
-                BlogPage(),
-                VedioPage(),
-                ImagePage(),
+              children: [
+                BlogPage(
+                  blogText: blogText,
+                ),
+                const VedioPage(),
+                const ImagePage(),
               ],
             ),
           ),
